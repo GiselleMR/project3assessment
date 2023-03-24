9c98f7494a16249bc2ffdf63a410b4764050c181
GiselleMR <giselle.rubin1@gmail.com>
2023-03-24 15:12:39 -0400

    add base

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
 class Item(models.Model):
-    description = models.CharField(max_length=300)
+    description = models.TextField(max_length=300)
+    def __str__(self):
+        return self.title
